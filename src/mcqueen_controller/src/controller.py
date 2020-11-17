ed0bfc8df27116154143f5a430a743885fbb9fa5
calhep <callumahepworth@gmail.com>
2020-11-17 15:28:01 -0800

    Classified controller.

--- src/mcqueen_controller/src/controller.py
+++ src/mcqueen_controller/src/controller.py
@@ -1,19 +1,51 @@
 #! /usr/bin/env python
 
+import roslib
+roslib.load_manifest('mcqueen_controller')
+
 import rospy
+import sys
+import cv2
+from geometry_msgs.msg import Twist
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
+# class image_converter:
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+#     def __init__(self):
+#         self.bridge = CvBridge()
+#         self.image_sub = rospy.Subscriber('/robot/camera1/image_raw',Image,self.callback)
+    
+#     def callback(self, image):
+#         try:
+#             cv_image = self.bridge.imgmsg_to_cv2(image, 'mono8') # image grayscaled
+#         except CvBridgeError as e:
+#             print(e)
 
+#         cv2.imshow("Image window", cv_image)
+#         cv2.waitKey(3)
 
-def listener():
-    rospy.init_node('clock_listener', anonymous=True)
-    rospy.Subscriber('/clock', String, callback)
+class robot_movement:
+
+    def __init__(self):
+        self.move_pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
+
+    def move_robot(self):
+        move = Twist()
+        move.linear.x = 0.1
+
+        self.move_pub.publish(move)
+
+def main(args):
+    rm = robot_movement()
+    rospy.init_node('controller')
+    rate = rospy.Rate(2)
+    
+    while not rospy.is_shutdown():
+        rm.move_robot()
+        rate.sleep()
 
 
 if __name__ == '__main__':
-    rate = rospy.Rate(2)
-    listener()
-    rospy.spin()
+    main(sys.argv)
