3da9373f80b2ba9023c4375c9c6738758d92f75e
calhep <callumahepworth@gmail.com>
2020-11-25 23:59:26 -0800

    Minor changes.

--- src/mcqueen_controller/src/callback_handling.py
+++ src/mcqueen_controller/src/callback_handling.py
@@ -21,8 +21,9 @@ CAMERA_HEIGHT = 720
 LIMIT = 710
 LEFT_BOUND = 480
 RIGHT_BOUND = 800
+RED_THRESHOLD = 0.8*255
 
-# Call this method before we move to check for pedestrian.
+# Method that determines whether there is a crosswalk immediately in front of the robot
 def is_at_crosswalk(colored_img):
 
     # get bottom row of frame
@@ -37,18 +38,19 @@ def is_at_crosswalk(colored_img):
     red_mask = cv2.inRange(hsv,lower_red,upper_red)
     avg_red = np.sum(red_mask) / ((CAMERA_HEIGHT - LIMIT)*(RIGHT_BOUND-LEFT_BOUND))
 
-    return avg_red > 0.8*255
+    return avg_red > RED_THRESHOLD
 
-
+# Method to generate the center of mass of a given cv2 image
 def generate_com(grayscale_img, prev_com):
     _, threshed_img = cv2.threshold(grayscale_img,
                                     LINE_THRESHOLD, 
                                     INTENSITY, 
                                     cv2.THRESH_BINARY) 
-                                    
+
     # Compute center of mass of the threshed image
     com = center_of_mass(threshed_img)
 
+    # 
     if isnan(com[0]) or isnan(com[1]):
         com_loc = prev_com
     else:
