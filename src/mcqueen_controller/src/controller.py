9373eb81414afaf2c9b0071e616fb7bde1bf6c01
calhep <callumahepworth@gmail.com>
2020-11-25 23:26:01 -0800

    Initial refactoring.

--- src/mcqueen_controller/src/controller.py
+++ src/mcqueen_controller/src/controller.py
@@ -9,6 +9,7 @@ import cv2
 import numpy as np
 
 import homography
+import callback_handling as ch
 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
@@ -40,21 +41,14 @@ class ImageConverter:
         except CvBridgeError as e:
             print(e)
 
-        # Threshold a vertical slice of the camera feed on the right side
-        # camera_img[y:,x:] (row,col)
-        _, threshed_img = cv2.threshold(grayscale_img[650:,900:],
-                                        self.LINE_THRESHOLD, 
-                                        self.INTENSITY, 
-                                        cv2.THRESH_BINARY) 
-
-        # Compute center of mass of the threshed image
-        my_com = self.generate_com(threshed_img)
-        x = my_com[0]
-        y = my_com[1]
-        #print(my_com)
-
         # If the red bar of crosswalk is detected, check for pedestrian
-        self.crosswalk(colored_img)
+        if ch.is_at_crosswalk(colored_img):
+            self.rm.stop_robot()
+            rospy.sleep(5)
+            # TODO: Manual control of robot here based on homography/color masking of pedestrians
+            self.rm.move_robot(x=0.05)
+
+        x, y, self.prev_com = ch.generate_com(grayscale_img[650:,900:], self.prev_com)
 
         # Control conditions
         if x < 120:
@@ -63,47 +57,6 @@ class ImageConverter:
             self.rm.move_robot(x=0.125, z=0)
         else:
             self.rm.move_robot(x=0., z=-.45)
-
-
-    # Call this method before we move to check for pedestrian.
-    def crosswalk(self, colored_img):
-
-        LIMIT = 710
-        LEFT_BOUND = 480
-        RIGHT_BOUND = 800
-        
-        # get bottom row of frame
-        bottom_row = colored_img[LIMIT:, LEFT_BOUND:RIGHT_BOUND]
-        hsv = cv2.cvtColor(bottom_row, cv2.COLOR_BGR2HSV)
-
-        # boundaries for red
-        lower_red = np.array([0,100,100])
-        upper_red = np.array([0,255,255])
-
-        # mask the red pixels and compute the average amount of red
-        red_mask = cv2.inRange(hsv,lower_red,upper_red)
-        avg_red = np.sum(red_mask) / ((self.CAMERA_HEIGHT - LIMIT)*(RIGHT_BOUND-LEFT_BOUND))
-
-        # If we are more than 80% red, and we are not currently in a crosswalk,
-        # that means stop and wait for pedestrian to cross, then go.
-        if (avg_red > 0.8*255):
-            self.rm.stop_robot()
-            rospy.sleep(5)
-            # TODO: Manual control of robot here based on homography/color masking of pedestrians
-            self.rm.move_robot(x=0.05)
-
-
-    # Returns CoM as a tuple, x coordinate in the 0th index and y coordinate in the 1st index
-    def generate_com(self, image):
-        com = center_of_mass(image)
-
-        if isnan(com[0]) or isnan(com[1]):
-            com_loc = self.prev_com
-        else:
-            com_loc = (int(com[1]), int(com[0]))
-            self.prev_com = com_loc
-
-        return com_loc
 
 
 class RobotMovement:
