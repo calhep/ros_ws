c010a710c5427aafbb37315192e3a355048a2e03
fizzer <jed324@gmail.com>
2020-11-26 16:55:28 -0800

    model.py

--- src/cnn_trainer/src/model/model.py
+++ src/cnn_trainer/src/model/model.py
@@ -6,17 +6,26 @@ import os
 
 from string import ascii_lowercase as LC
 from matplotlib import pyplot as plt
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras import backend
+from keras.utils import plot_model
+
 
 PATH = '/home/fizzer/ros_ws/src/cnn_trainer'
 PLATE_DIR = os.path.join(PATH, 'media', 'plates')
+MODEL_PATH = os.path.join(PATH, 'src', 'model')
 
 VALIDATION_SPLIT = 0.2
 
 
+# Get all files in a path
 def files_in_folder(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 
+# Generate a one hot vector for a given character
 def one_hot(c):
     vec = [0] * 36
     try:
@@ -29,6 +38,14 @@ def one_hot(c):
     return vec
 
 
+# Return an alphanumeric character from a given index
+def index_to_val(i):
+    abc123 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    
+    return abc123[i]
+
+
+# Return the 4 partitions of a plate image and their associated one hot vectors
 def process_plate(my_file):
     plate_path = os.path.join(PLATE_DIR, my_file)
     plate_img = cv2.imread(plate_path)
@@ -47,7 +64,8 @@ def process_plate(my_file):
     return imgs, vecs
 
 
-def generate_dataset():
+# Return the datasets associated with the processed plates and their one-hot vectors
+def get_dataset():
     plates = files_in_folder(PLATE_DIR)
 
     X_images = []
@@ -64,8 +82,38 @@ def generate_dataset():
     return X_dataset, Y_dataset
 
 
-def main():
-    X_dataset, Y_dataset = generate_dataset()
+# Save a Keras model
+def save_model(model):
+    model.save('keras/my_model')
+    return
+
+# Load a Keras model
+def load_model():
+    return models.load_model('keras/my_model')
+
+
+# Generate completely new training model
+def generate_model(lr):
+    conv_model = models.Sequential()
+    conv_model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(300, 105, 3)))
+    conv_model.add(layers.MaxPooling2D((2, 2)))
+    conv_model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    conv_model.add(layers.MaxPooling2D((2, 2)))
+    conv_model.add(layers.Flatten())
+    conv_model.add(layers.Dropout(0.5))
+    conv_model.add(layers.Dense(512, activation='relu'))
+    conv_model.add(layers.Dense(36, activation='softmax'))
+
+    # Set Learning Rate and Compile Model
+    conv_model.compile(loss='categorical_crossentropy',
+                    optimizer=optimizers.RMSprop(lr=lr),
+                    metrics=['acc'])
+
+    return conv_model
+
+
+# Get a model either by generating a new one or load from local
+def get_model(X_dataset, Y_dataset, lr=1e-4, print_summary=False, new=False):  
     
     print("Total examples: {}\nTraining examples: {}\nTest examples: {}".
             format(X_dataset.shape[0], 
@@ -74,7 +122,86 @@ def main():
     print("X shape: " + str(X_dataset.shape))
     print("Y shape: " + str(Y_dataset.shape))
 
-    # TODO: CNN training here
+    # Create a new model and save it or load one locally.
+    if new:
+        conv_model = generate_model(lr)
+       # save_model(conv_model)
+    else:
+        conv_model = load_model()
+
+    conv_model.summary()
+
+    return conv_model
+
+
+# Predict a plate using a model
+def predict_plate(plate, model):
+    imgs, vecs = process_plate(plate)
+    dataset = np.array(imgs) / 255
+
+    char0 = np.array(imgs[0])
+    char1 = np.array(imgs[1])
+    char2 = np.array(imgs[2])
+    char3 = np.array(imgs[3])
+    total_plate = np.concatenate((char0,char1,char2,char3),axis=1)
+
+    chars = []
+
+    for i in range(4):
+        image = np.expand_dims(dataset[i], axis=0)
+
+        y_true = vecs[i]
+        index_true = np.argmax(y_true)
+
+        y_predicted = model.predict(image)[0]
+        index_predicted = np.argmax(y_predicted)
+
+        chars.append(index_to_val(index_predicted))
+
+    print("Predicted:", chars) 
+
+
+def main():
+    # PARAMETERS TO ADJUST
+    TRAIN = True
+    PRINT_HISTORY = True
+    LEARNING_RATE = 1e-5
+    EPOCHS = 5
+
+    # Get datasets and choose whether to generate a new model or load a model
+    X_dataset, Y_dataset = get_dataset()
+    conv_model = get_model(X_dataset, Y_dataset, lr=LEARNING_RATE, new=True)
+    
+    if TRAIN:
+        history_conv = conv_model.fit(X_dataset, Y_dataset, 
+                                    validation_split=VALIDATION_SPLIT, 
+                                    epochs=EPOCHS, 
+                                    batch_size=16)
+
+    # Fit the data and get history of the model over time, if specified
+    if PRINT_HISTORY:
+        plt.plot(history_conv.history['loss'])
+        plt.plot(history_conv.history['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Training loss', 'Validation loss'], loc='upper right')
+        plt.show()
+
+        plt.plot(history_conv.history['acc'])
+        plt.plot(history_conv.history['val_acc'])
+        plt.title('Model Accuracy')
+        plt.ylabel('Accuracy (%)')
+        plt.xlabel('Epoch')
+        plt.legend(['Train accuracy', 'Val accuracy'], loc='lower right')
+        plt.show()
+
+    # Testing the model
+    plates = files_in_folder(PLATE_DIR)
+    print("Testing:", plates[0])
+    plate_to_test = plates[0]
+
+    predict_plate(plate_to_test, conv_model)
 
 
 if __name__ == '__main__':
